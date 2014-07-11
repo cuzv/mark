e041b1d0201208b40ea87b44798bc71a6226faf0
atcuan <atcuan@gmail.com>
2014-07-11 16:29:34 +0800

    Apple Push Notification Services Tutorial 1/2

---
+++ ios/SimplePush/simplepush.php
@@ -0,0 +1,49 @@
+<?php
+
+// Put your device token here (without spaces):
+$deviceToken = 'f9a692ea2ea56513ed8d18669cca4252e0a821f99840768d585f294542296969';
+
+// Put your private key's passphrase here:
+$passphrase = 'chinou';
+
+// Put your alert message here:
+$message = 'My first push notification!';
+
+////////////////////////////////////////////////////////////////////////////////
+
+$ctx = stream_context_create();
+stream_context_set_option($ctx, 'ssl', 'local_cert', 'ck.pem');
+stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+// Open a connection to the APNS server
+$fp = stream_socket_client(
+	'ssl://gateway.sandbox.push.apple.com:2195', $err,
+	$errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+
+if (!$fp)
+	exit("Failed to connect: $err $errstr" . PHP_EOL);
+
+echo 'Connected to APNS' . PHP_EOL;
+
+// Create the payload body
+$body['aps'] = array(
+	'alert' => $message,
+	'sound' => 'default'
+	);
+
+// Encode the payload as JSON
+$payload = json_encode($body);
+
+// Build the binary notification
+$msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+
+// Send it to the server
+$result = fwrite($fp, $msg, strlen($msg));
+
+if (!$result)
+	echo 'Message not delivered' . PHP_EOL;
+else
+	echo 'Message successfully delivered' . PHP_EOL;
+
+// Close the connection to the server
+fclose($fp);
